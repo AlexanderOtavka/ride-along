16f8838021e117e51df4198b5de81ea43dbe234a
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-06-02 21:51:19 -0700

    Add license

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,13 +1,35 @@
-// In production, we register a service worker to serve assets from local cache.
+/**
+ * @file registerServiceWorker.tsx
+ *
+ * Created by Zander Otavka on 6/2/17.
+ * Copyright (C) 2016  Grinnell AppDev.
+ *
+ * @license
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on the "N+1" visit to a page, since previously
-// cached resources are updated in the background.
-
-// To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
-// This link also includes instructions on opting out of this behavior.
-
+/**
+ * In production, we register a service worker to serve assets from local cache.
+ *
+ * This lets the app load faster on subsequent visits in production, and gives
+ * it offline capabilities. However, it also means that developers (and users)
+ * will only see deployed updates on the "N+1" visit to a page, since previously
+ * cached resources are updated in the background.
+ *
+ * To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
+ * This link also includes instructions on opting out of this behavior.
+ */
 export default function register() {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     window.addEventListener("load", () => {
