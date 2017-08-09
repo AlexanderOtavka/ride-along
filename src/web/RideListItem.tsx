c6cd364d467ecbee4a33bc08e150b78c66142f3e
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-08-09 15:53:08 -0700

    Remove unused import

--- src/web/RideListItem.tsx
+++ src/web/RideListItem.tsx
@@ -24,7 +24,6 @@ import { Link } from "react-router-dom"
 import classnames from "classnames"
 import leftPad from "left-pad"
 
-import Ride from "./Ride"
 import RideSection from "./RideSection"
 
 import styles from "./RideListItem.sass"
